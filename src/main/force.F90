f2d5869fc9bce68aa3593e3698a2c2ee331ed731
zachboots <146029428+zachboots@users.noreply.github.com>
2025-02-04 01:13:30 -0330

    set cooling off when icooling=0

--- src/main/force.F90
+++ src/main/force.F90
@@ -2982,7 +2982,7 @@ subroutine finish_cell_and_store_results(icall,cell,fxyzu,xyzh,vxyzu,poten,dt,dv
        endif
 
        ! cooling timestep dt < fac*u/(du/dt)
-       if (maxvxyzu >= 4 .and. .not. gr) then ! not with gr which uses entropy
+       if (icooling > 0 .and. maxvxyzu >= 4 .and. .not. gr) then ! not with gr which uses entropy
           if (eni + dtc*fxyzu(4,i) < epsilon(0.) .and. eni > epsilon(0.)) dtcool = C_cool*abs(eni/fxyzu(4,i))
        endif
 
